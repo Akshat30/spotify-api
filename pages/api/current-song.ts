0fa46ac24dc6c2dab88af30d83515061d40c29b8
Akshat30 <akshat30@gmail.com>
2023-09-15 11:46:09 -0700

    testing current song

--- pages/api/current-song.ts
+++ pages/api/current-song.ts
@@ -1,10 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
+import { authOptions } from "./auth/[...nextauth]";
+import { getServerSession } from "next-auth";
 
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const sess = await getSession({ req });
+  const sess = await getServerSession(req, res, authOptions);
   const accesstoken = sess?.accessToken;
 
   if (req.method === "GET") {
