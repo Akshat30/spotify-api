b26432b95593000bea1e5d13a6d57851d8155dc9
Akshat30 <akshat30@gmail.com>
2023-09-16 08:37:15 -0700

    Update top-songs.ts

--- pages/api/top-songs.ts
+++ pages/api/top-songs.ts
@@ -3,14 +3,14 @@ import { getSession } from "next-auth/react";
 import { authOptions } from "./auth/[...nextauth]";
 import { getServerSession } from "next-auth";
 
-const TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/top/artists`;
+const TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/top/tracks`;
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const sess = await getServerSession(req, res, authOptions);
   const accesstoken = sess?.accessToken;
 
   if (req.method === "GET") {
-    const response = await getNowPlaying(accesstoken!);
+    const response = await getTopSongs(accesstoken!);
     const parsed = await response.json();
 
     return res.status(200).json(parsed.items);
@@ -19,16 +19,13 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
 export default handler;
 
-export const getNowPlaying = async (refresh_token: string) => {
+export const getTopSongs = async (refresh_token: string) => {
   try {
     // Make the request to the Spotify API with the new access token
     const response = await fetch(TRACKS_ENDPOINT, {
       headers: {
         Authorization: `Bearer ${refresh_token}`,
       },
-      body: JSON.stringify({
-        limit: 10,
-      }),
     });
 
     if (!response.ok) {
