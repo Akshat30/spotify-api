967de5c4047ce72aca506f195931532d427a73f8
Akshat30 <akshat30@gmail.com>
2023-09-17 13:34:59 -0700

    different song time ranges

--- pages/api/top-songs.ts
+++ pages/api/top-songs.ts
@@ -6,11 +6,20 @@ import { getServerSession } from "next-auth";
 const TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/top/tracks`;
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const query = req.query;
+  let time;
+
+  if (Array.isArray(query.time)) {
+    time = query.time[0];
+  } else {
+    time = query.time;
+  }
+
   const sess = await getServerSession(req, res, authOptions);
   const accesstoken = sess?.accessToken;
 
   if (req.method === "GET") {
-    const response = await getTopSongs(accesstoken!);
+    const response = await getTopSongs(accesstoken!, time!);
     const parsed = await response.json();
 
     return res.status(200).json(parsed.items);
@@ -19,14 +28,17 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
 export default handler;
 
-export const getTopSongs = async (refresh_token: string) => {
+export const getTopSongs = async (refresh_token: string, time: string) => {
   try {
     // Make the request to the Spotify API with the new access token
-    const response = await fetch(TRACKS_ENDPOINT+"?limit=10&time_range=long_term", {
-      headers: {
-        Authorization: `Bearer ${refresh_token}`,
-      },
-    });
+    const response = await fetch(
+      TRACKS_ENDPOINT + "?limit=10&time_range=" + time,
+      {
+        headers: {
+          Authorization: `Bearer ${refresh_token}`,
+        },
+      }
+    );
 
     if (!response.ok) {
       // Handle error response here if needed
--- pages/topsongs.tsx
+++ pages/topsongs.tsx
@@ -7,9 +7,10 @@ export default function Home() {
   const session = useSession();
 
   const [songData, setSongData] = useState<any | null>(null); // Initialize songData state
+  const [time, setTime] = useState<string | null>(null);
 
   useEffect(() => {
-    fetch("/api/top-songs")
+    fetch("/api/top-songs" + "?time=" + time)
       .then((response) => {
         if (!response.ok) {
           throw new Error(`Request failed with status: ${response.status}`);
@@ -23,7 +24,11 @@ export default function Home() {
       .catch((error) => {
         console.error("Error fetching recommendations:", error);
       });
-  }, []); // Run this effect whenever the 'data' changes
+  }, [time]); // Run this effect whenever the 'data' changes
+
+  const handleTimeChange = (selectedTime: string) => {
+    setTime(selectedTime);
+  };
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
@@ -37,6 +42,64 @@ export default function Home() {
       <div>
         {session.status === "authenticated" ? (
           <div className="mt-4">
+            <div className="flex items-center">
+              <div className="relative inline-block text-left">
+                <div>
+                  <button
+                    type="button"
+                    className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+                    id="dropdown-menu"
+                    aria-haspopup="true"
+                    onClick={() => {}}
+                  >
+                    Select Time Period
+                    <svg
+                      className="-mr-1 ml-2 h-5 w-5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10.293 6.293a1 1 0 011.414 0l3 3a1 1 0 01-1.414 1.414L11 8.414V16a1 1 0 11-2 0V8.414L6.707 10.707a1 1 0 01-1.414-1.414l3-3z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </div>
+                <div
+                  className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="dropdown-menu"
+                >
+                  <div className="py-1" role="none">
+                    <button
+                      onClick={() => handleTimeChange("short_term")}
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                      role="menuitem"
+                    >
+                      Short Term
+                    </button>
+                    <button
+                      onClick={() => handleTimeChange("medium_term")}
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                      role="menuitem"
+                    >
+                      Medium Term
+                    </button>
+                    <button
+                      onClick={() => handleTimeChange("long_term")}
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                      role="menuitem"
+                    >
+                      Long Term
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
             <div className="flex flex-col mt-8 items-center justify-center mb-12">
               {songData?.map((track: any, index: any) => (
                 <div key={index} className="mt-2">
